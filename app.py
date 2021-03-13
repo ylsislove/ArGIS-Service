7b18b6cf1320d21e65bf4af1a215e4921af89241
wangyu <916491013@qq.com>
2021-03-13 20:10:05 +0800

    Update：app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ streamer = ThreadedStreamer(batch_prediction, batch_size=64)
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        file = request.files['file']
+        file = request.files['image']
         img_bytes = file.read()
         low, mlow, middle, mhigh, high, other = get_prediction(image_bytes=img_bytes)
         return jsonify({'低收入': low, '中低收入': mlow, '中等收入': middle, '中高收入': mhigh, '高收入': high, '其他': other,})
@@ -19,7 +19,7 @@ def predict():
 @app.route('/stream_predict', methods=['POST'])
 def stream_predict():
     if request.method == 'POST':
-        file = request.files['file']
+        file = request.files['image']
         img_bytes = file.read()
         low, mlow, middle, mhigh, high, other = streamer.predict([img_bytes])[0]
         return jsonify({'低收入': low, '中低收入': mlow, '中等收入': middle, '中高收入': mhigh, '高收入': high, '其他': other,})
